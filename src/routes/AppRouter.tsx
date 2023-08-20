b5cdbd97f3a50a61c6f2903889913b4f9b2c4341
Francisco Carrozzino <fjcarrozzino@gmail.com>
2023-08-20 21:14:30 +0100

    rutas

---
+++ src/routes/AppRouter.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NavBar } from '../components/NavBar/NavBar'
+
+const AppRouter = () => {
+  return (
+    <>
+    <NavBar/>
+    </>
+  )
+}
+
+export default AppRouter
